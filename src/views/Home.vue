a1078705f6244e74dec0e17c6179e2df35e970a7
ZihaoGuan <grand_zh@yeah.net>
2020-11-17 12:23:29 +1300

    modify user view, now show user info on user view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class = "container">
     <div class = "row justify-content-center">
       <div class = "col-6">
+        <h2>
+          USERS
+        </h2>
         <ul class = "list-group">
           <li class = "list-group-item d-flex justify-content-between"
           v-for="user in users"
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,7 +1,39 @@
 <template>
   <div class = "container">
-    <div class = "row justify-content-center">
+    <div class = "row">
       <div class = "col">
+        <div class = "jumbotron text-left" v-if="user != undefined">
+          <h2>
+            {{user.name}}
+          </h2>
+          <p class = "">
+            <b>ID:</b> {{user.id}}
+          </p>
+          <p>
+            <b>username:</b> {{user.username}}
+          </p>
+          <p>
+            <b>email:</b> {{user.email}}
+          </p>
+          <p>
+            <b>address:</b>
+            <br>
+            {{user.address.street}}<br>
+            {{user.address.suite}}<br>
+            {{user.address.city}}<br>
+            {{user.address.zipcode}}<br>
+          </p>
+          <p>
+            <b>phone:</b> {{user.phone}}
+          </p>
+          <p>
+            <b>website:</b> {{user.website}}
+          </p>
+        </div>
+        <hr>
+        <h1>
+          POSTS
+        </h1>
         <ul class = "list-group">
           <li class = "list-group-item text-left"
           v-for="post in postList"
@@ -21,14 +53,19 @@ export default {
   name: 'User',
   computed: {
     ...mapState([
-      'posts'
+      'posts',
+      'users'
     ]),
     postList: function () {
       return this.posts.filter(this.checkId).sort((a, b) => (a.id < b.id) ? 1 : -1).slice(0, 10)
+    },
+    user: function () {
+      return this.users.find((user) => user.id.toString() === this.userId.toString())
     }
   },
   data: function () {
     return {
+      userId: this.$route.params.id
     }
   },
   methods: {
@@ -38,9 +75,6 @@ export default {
     goToPath (id) {
       this.$router.push({ name: 'post', params: { id } })
     }
-  },
-  mounted: function () {
-    console.log('user mounted')
   }
 }
 
